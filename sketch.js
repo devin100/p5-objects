3888c7cdda2bf2b568c66b58918b0b148b44b0bc
devin williams <dwilliams@wlapcs.org>
2017-03-28 19:13:45 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,58 @@
+function setup(){
+    createCanvas(600,400)
+    p1 = new pacman(300, 200)
+}
+
+
+function draw(){
+    background(0);
+    p1.display();
+p1.move();
+}
+
+function pacman(x, y){
+    this.x = 300;
+    this.y = 200;
+    this.radB = 30;
+    this.radT = 305;
+    this.rotation = 0;
+    this.display = function(){
+        fill(255,255,0)
+        //ellipse(this.x,this.y, 40, 40)
+        arc(this.x, this.y, 40, 40, radians(this.radB + this.rotation), 
+        radians(this.radT + this.rotation), PIE);
+    };
+    this.move = function(){
+        if(KeyIsDown(UP_ARROW)){
+          this.y -= 5;  
+          this.rotation = 270;
+        }
+        if(KeyIsDown(DOWN_ARROW)){
+          this.y += 5;
+          this.rotation = 90;
+        }
+        if(KeyIsDown(RIGHT_ARROW)){
+          this.x += 5;
+          this.rotation = 0;
+        }
+        if(KeyIsDown(LEFT_ARROW)){
+          this.x -= 5;
+          this.rotation = 180
+        }
+    };
+        this.boundary = function(){
+            if(this.x > width)
+            this.x = 0;
+        }
+        if(this.x < 0){
+           this.x = width
+        }
+        if(this.y > height){
+           this.y = 0;
+         }
+         if(this.y < 0){
+             this.y = height;
+         }
+    
+    }
+
